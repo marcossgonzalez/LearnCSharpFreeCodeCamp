83f6f915d4869ae0eaba16bcc6fe9eaf27b3ea8b
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-12-22 01:12:48 -0300

    Lesson 1 Update
    Lesson 1 Update

---
+++ Learning C#/Program.cs
@@ -0,0 +1,15 @@
+﻿//Console.WriteLine("Hello World!");
+Console.WriteLine("Congratulations!");
+Console.WriteLine(" ");
+Console.WriteLine("You wrote your first lines of code.");
+//The period is called ember access operator; it`s how you navigate
+//from the class to one of its methods. While 'WriteLine' is a method. And
+//finally, the 'Console' is the class in which the method writeline is called
+//to print the words that we want.
+//Remember: WriteLine() method prints the output with a new line after it, while
+//Write() won't.
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
+//Using both techniques, this is what we get.
